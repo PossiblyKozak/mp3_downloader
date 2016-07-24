2e96a6f8dcc150ae42cfd0a187b2225978a6c87f
PossiblyKozak <a.kozak1@outlook.com>
2016-07-23 22:07:04 -0400

    Major update, added way more comprehensive information while downloading, indication of success, timing estimates, and retries in order to minimize song fetch failure.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Mp3_Downloader
-A simple python script from which you can download the songs by just entering the name. You can also download by entering a list of name, youtube link or list of youtube links.
+A simple python script from which you can download the songs by just entering the name. 
+You can also download by entering a list of names, youtube link or list of youtube links.
 
 
 Python version :- Python 3.4.3+
@@ -14,7 +15,7 @@ You can try that online. Here is the link ->  http://songs.seemple.in
 
 # Revisions by Michael Gilbert:
 
-I have ported the entire script to be compatible with BOTH python 3
+I have ported the entire script to be compatible with BOTH python 2
 AND python 3.
 
 All functionality is kept between the 2 versions of python. There is a 
@@ -24,3 +25,11 @@ this process up.
 I also need to create a function for a progress bar. You do not get one
 when using urllib, only when using wget. Without wget, you have a more
 portable script.
+
+# Revisions by Alex Kozak
+
+I have made it so that you can see what's going on from a console viewpoint
+
+Added threading
+
+Checking downloaded files for files under 1MB, and retrying downloads
--- program.py
+++ program.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # A python to download a song or a list of songs.
-# create by Aman Roy
+# created by Aman Roy
 # Creation Date : 18-Feb-2016
+# Forked on July 23rd 2016
+# Forked by Alex Kozak
 # Python version used : - Python 3.4.3+ (ported for python2)
 # Please use right spelling to avoid errors
 # All licence belongs to authour.
@@ -11,12 +13,16 @@ import re
 import urllib
 import os
 import sys
+import threading
+import time
+from multiprocessing.dummy import Pool as ThreadPool 
 
+global finishedDownload
+finishedDownload = 0
 
 # determine python version
+getSize = os.path.getsize
 version = sys.version_info[0]
-
-
 # set user_input for correct version of python
 if version == 2:  # python 2.x
     user_input = raw_input
@@ -25,7 +31,6 @@ if version == 2:  # python 2.x
     encode = urllib.urlencode  # encode a search line
     retrieve = urllib.urlretrieve  # retrieve url info
     cleanup = urllib.urlcleanup()  # cleanup url cache
-    
 else:  # python 3.x
     user_input = input
     import urllib.request
@@ -34,7 +39,6 @@ else:  # python 3.x
     encode = urllib.parse.urlencode
     retrieve = urllib.request.urlretrieve
     cleanup = urllib.request.urlcleanup()
-
 
 
 # clear the terminal screen
@@ -58,14 +62,14 @@ def video_title(url):
 # the intro to the script
 def intro():
     print('''Created by Aman Roy
-    FB:- amanroy007
-    Email:- royaman8757@gmail.com''')
+    Forked by Alex Kozak
+    e-mail: a.kozak1@outlook.com''')
 
 
 # find out what the user wants to do
 def prompt():
     # userr prompt to ask mode
-    print ('''\t\t\t Select A mode  
+    print ('''Select A mode  
     [1] Download from a list
     [2] Download from direct entry
     [3] Download from the youtube link
@@ -75,134 +79,203 @@ def prompt():
     choice = user_input('>>> ')
     return str(choice)
 
-
+class nameThread (threading.Thread):
+    def __init__(self, threadID, name, songName, numSong):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.songName = songName
+        self.numSong = numSong
+    def run(self):
+        single_name_download(self.songName, self.numSong, "", 0)
+        
+class linkThread (threading.Thread):
+    def __init__(self, threadID, youtubeLink):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.youtubeLink = youtubeLink
+    def run(self):
+        link_download(self.youtubeLink, 0)
+        
 # download from a list
-def name_list_download():
-    fileName = user_input('fileName(with extension): ')  # get the file name to be opened
-    
+def name_list_download():   
+    fileName = user_input('Please enter the File Name With extension (i.e. ".txt")')  # get the file name to be opened
+    a = 0
+    numSong = 0
     # find the file and set fhand as handler
     try:
         fhand = open(fileName, 'r')
     except IOError:
         print('File does not exist')
-        exit(1)
-
+        exit(1)      
+    sn = []
+    screen_clear()
     for songs in fhand:
-        query_string = encode({'search_query' : songs})
-        html_content = urlopen('http://www.youtube.com/results?' + query_string)
+        songs = songs.strip().replace('\n','').replace('/','')
+        numSong = numSong + 1
+        sn.append(songs)
         
-        if version == 3:  # if using python 3.x
-            search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-        else:  # if using python 2.x
-            search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read())
-                
-        # generate a download link that can grab the audio file using youtube2mp3 API
-        downloadLinkOnly = 'http://www.youtubeinmp3.com/fetch/?video=' + 'http://www.youtube.com/watch?v=' + search_results[0]
-        # why do we have this variable being created?
-        downloadLink = songs + ':' + downloadLinkOnly  # song name and link in a single file to save it in the file safely
-                 
-        try:
-            print('Downloading %s' % songs) 
-            # code a progress bar for visuals?? this way is more portable than wget
-            retrieve(downloadLinkOnly, filename='%s.mp3' % songs)  # download the file in the working directory
-            cleanup  # clear the cache created by urlretrieve
-        except:
-            print('Error downloading %s' % songs)
+    print("There are %s songs in the file provided" % numSong)
+    print("The download should take about %s minute(s) and %s seconds" % (((numSong*10+120)//60), ((numSong*10+120)%60)))
+    a = input("Press Enter to Begin Download")
+    a = 0        
+    print("Beginning download...\n")
+    for songs in sn:    
+        thread = nameThread(a, songs, songs, numSong)       
+        thread.start()
+        time.sleep(2)
+        a = a + 1       
     fhand.close()
 
 
 # download directly with a song name
-def single_name_download():
-    song = user_input('Enter the song name: ')  # get the song name from user
-    
-        # try to get the search result and exit upon error
-    try:
-        query_string = encode({"search_query" : song})
-        html_content = urlopen("http://www.youtube.com/results?" + query_string)
-        
-        if version == 3:  # if using python 3.x
-            search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-        else:  # if using python 2.x
-            search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read())
-    except:
-        print('Network Error')
-        exit(1)
-
-    # generate a download link that can be used to get the audio file using youtube2mp3 API
-    downloadLinkOnly = 'http://www.youtubeinmp3.com/fetch/?video=' + 'http://www.youtube.com/watch?v=' + search_results[0]
-    try:
-        print('Downloading %s' % song)
-        # code a progress bar for visuals? this way is more portable than wget
-        retrieve(downloadLinkOnly, filename='%s.mp3' % song)
-        cleanup  # clear the cache created by urlretrieve
-    except:
-        print('Error downloading %s' % song)
-        exit(1)
+def single_name_download(song, numSongs, downloadLinkOnly, retries):
+    global finishedDownload
+    if retries < 3:
+        if song == "":
+            song = user_input('Enter the song name: ')  # get the song name from user
+        succ = True
+            # try to get the search result and exit upon error
+        if os.path.isfile("%s.mp3" % song) == False:         
+            if downloadLinkOnly == "":
+                try:
+                    print("Searching for %s..." % song)
+                    query_string = encode({"search_query" : song})
+                    html_content = urlopen("http://www.youtube.com/results?" + query_string)
+                    print(" - Found %s." % song)
+                    if version == 3:  # if using python 3.x
+                        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+                    else:  # if using python 2.x
+                        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read())
+                except:
+                    print(' - Network Error Downloading %s' % song) 
+                    succ = False
+            if succ == True:
+                # generate a download link that can be used to get the audio file using youtube2mp3 API
+                x = False                
+                if downloadLinkOnly == "":
+                    downloadLinkOnly = 'http://www.youtubeinmp3.com/fetch/?video=' + 'http://www.youtube.com/watch?v=' + search_results[0]
+                    #downloadLinkOnly = "http://serve01.mp3skull.onl/get?id=" + search_results[0]
+                try:
+                    print(' - Downloading %s' % song)
+                    # code a progress bar for visuals? this way is more portable than wget
+                    retrieve(downloadLinkOnly, filename='%s.mp3' % song)
+                    cleanup  # clear the cache created by urlretrieve
+                    x = True
+                    print("The file '%s.mp3' is %sMB" % (song, round(getSize("%s.mp3" % song)/1048576, 1)))
+                    if getSize ("%s.mp3" % song) < 1048576:
+                        print("%s downloaded incorrectly" % song)
+                        os.remove("%s.mp3" % song)
+                        single_name_download(song, numSongs, downloadLinkOnly, retries + 1)
+                        x = False
+                except:
+                    print(' - Error downloading %s' % song)
+                    os.remove("%s.mp3" % song)
+                if x:
+                    finishedDownload = finishedDownload + 1
+                    print("   - %s Download Successful" % song)        
+                    print("%s / %s Songs downloaded" % (finishedDownload, numSongs))
+        else:
+            print("The song '%s.mp3 is already downloaded" % song)            
+            finishedDownload = finishedDownload + 1
+            print("%s / %s Songs downloaded" % (finishedDownload, numSongs))
+    else:
+        print ("Max retries reached for %s" % song)
+        finishedDownload = finishedDownload + 1
 
 
 # download directly with a youtube link
-def link_download():
-    print('Enter full youtube link (case sensitive)')
-    print('e.g. - https://www.youtube.com/watch?v=rYEDA3JcQqw')
-    youtubeLink = user_input('>>> ')
-    downloadLinkOnly = 'http://www.youtubeinmp3.com/fetch/?video=' + youtubeLink
-
-    try:
-        song = video_title(youtubeLink)
-        print('Downloading %s' % song)
-        # code a progress bar for visuals? this way is more portable than wget
-        retrieve(downloadLinkOnly, filename='%s.mp3' % song)
-        cleanup  # clear the cache created by urlretrieve
-    except:
-        print('Error downloading %s' % song)
-        exit(1)
-
+def link_download(youtubeLink, retries):
+    global finishedDownload    
+    if retries < 3:
+        if youtubeLink == "":
+            print('Enter full youtube link (case sensitive)')
+            print('e.g. - https://www.youtube.com/watch?v=rYEDA3JcQqw')
+            youtubeLink = user_input('>>> ')
+        succ = True
+            # try to get the search result and exit upon error
+        if os.path.isfile("%s.mp3" % song) == False:         
+            # generate a download link that can be used to get the audio file using youtube2mp3 API
+            x = False                
+            if youtubeLink == "":
+                downloadLinkOnly = 'http://www.youtubeinmp3.com/fetch/?video=' + youtubeLink
+            try:
+                print(' - Downloading %s' % song)
+                # code a progress bar for visuals? this way is more portable than wget
+                retrieve(downloadLinkOnly, filename='%s.mp3' % song)
+                cleanup  # clear the cache created by urlretrieve
+                x = True
+                print("The file '%s.mp3' is %sMB" % (song, round(getSize("%s.mp3" % song)/1048576, 1)))
+                if getSize ("%s.mp3" % song) < 1048576:
+                    print("%s downloaded incorrectly" % song)
+                    os.remove("%s.mp3" % song)
+                    link_download(youtubeLink, retries + 1)
+                    x = False
+            except:
+                print(' - Error downloading %s' % song)
+                os.remove("%s.mp3" % song)
+            if x:
+                finishedDownload = finishedDownload + 1
+                print("   - %s Download Successful" % song)        
+                print("%s / %s Songs downloaded" % (finishedDownload, numSongs))
+        else:
+            print("The song '%s.mp3 is already downloaded" % song)            
+            finishedDownload = finishedDownload + 1
+            print("%s / %s Songs downloaded" % (finishedDownload, numSongs))
+    else:
+        print ("Max retries reached for %s" % song)
+        finishedDownload = finishedDownload + 1
 
 # download songs with a list of youtube links
 def link_list_download():
-    fileName = user_input('fileName(with exension): ')  # get the file name to be opened
-    
+    fileName = user_input('Please enter the File Name With extension (i.e. ".txt")')  # get the file name to be opened
+    a = 0
+    numSong = 0
     # find the file and set fhand as handler
     try:
-        fhand = open(fileName, 'r')
+        fhand = open(fileName + ".txt", 'r')
     except IOError:
         print('File does not exist')
-        exit(1)
-    
-    for links in fhand:
-        try:
-            downloadLinkOnly = 'http://www.youtubeinmp3.com/fetch/?video=' + links
-            song = video_title(links)
-            print('Downloading %s' % song)
-            # code a progress bar for visuals? this way is more portable than wget
-            retrieve(downloadLinkOnly, filename='%s.mp3' % song)
-            cleanup  # clear the cache created by urlretrieve
-        except:
-            print('Error downloading %s' % song)
+        exit(1)      
+    sn = []
+    screen_clear()
+    for songs in fhand:
+        songs = songs.strip()
+        numSong = numSong + 1
+        sn.append(songs)
+        
+    print("There are %s songs in the file provided" % numSong)
+    print("The download should take about %s minute(s) and %s seconds" % (((numSong*10+120)//60), ((numSong*10+120)%60)))
+    a = input("Press Enter to Begin Download")
+    a = 0        
+    print("Beginning download...\n")
+    for songs in sn:    
+        thread = linkThread(a, songs)       
+        thread.start()
+        time.sleep(2)
+        a = a + 1       
     fhand.close()
 
 
 # program exit
 def exit(code):
-    print('\nExiting....')
-    print('\nHave a good day.')
     sys.exit(code)
 
 
 # main guts of the program
-def main():
+def main():    
+    finishedDownload = 0
     try:
         screen_clear()
-        intro()
         choice = prompt()
 
         try:
             if choice == '1':
                 name_list_download()
             elif choice == '2':
-                single_name_download()
+                single_name_download("", 1, "", 0)
             elif choice == '3':
-                link_download()
+                link_download("", 0)
             elif choice == '4':
                 link_list_download()
         except NameError:
