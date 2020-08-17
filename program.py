b51dbd16cc79630ff847f58112e868c6eb21b394
Alex Kozak <a.kozak1@outlook.com>
2020-08-17 16:09:59 -0400

    Circumvent the url request youtube changes
    add selenium to allow the request to actually generate the proper HTML, and allow the links to be gathered. Also fixed the playlist downloader to actually scrape properly and download all of the songs. Also fixed the settings changes

--- program.py
+++ program.py
@@ -9,6 +9,39 @@
 import re, urllib, os, sys, threading, time, urllib.request, urllib.parse, glob, youtube_dl, mutagen
 from mutagen.easyid3 import EasyID3
 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def getSingle(plUrl):
+    driver = webdriver.Chrome("./chromedriver.exe")
+    driver.get(plUrl)
+    elements = []
+    elems = driver.find_elements_by_xpath("//a[@id='video-title']")
+    print("Collecting link...")
+    res = str(elems[0].get_attribute("href"))
+    driver.quit()
+    return res
+
+def getPlaylist(plUrl):
+    driver = webdriver.Chrome("./chromedriver.exe")
+    driver.get(plUrl)
+    elements = []
+    elems = driver.find_elements_by_xpath("//a[@href]")
+    print("Collecting links...")
+    for elem in elems:
+        elements.append(str(elem.get_attribute("href")))
+
+    allLinks = set()
+    for i in elements:
+        if 'https://www.youtube.com/watch?' in i:
+            if '&' in i:
+                i = i.split('&')[0]
+            allLinks.add(i)
+            
+    driver.quit()
+    return allLinks
+
 class _Const(object):                       # Creation of a constant class, so that mass changes in the code can be done. These can now be changed from the settings in the client
     RETRIES = 5                 # Total number of retries the program undergoes before giving up 
     DIRECTORY = "Songs"         # The subdirectory name in which all the downloaded mp3's will reside.
@@ -62,7 +95,7 @@ def changeSettings():
         setIndex = input("Enter number of setting you want to change\n>>> ")
         if setIndex == 'x' or setIndex == 'X':  
             staySettings = False
-            setSettings()
+            Settings.sSet()
         elif str.isdigit(setIndex) and int(setIndex) < 6:   # if the str.isdigit wasn't there, the program would crash if a non-integer and not 'x' was entered
             newValue = input("Enter the new value\n>>> ")
             if int(setIndex) == 0:
@@ -170,16 +203,11 @@ def prompt():
 def youTubePlaylistDownloader():            # Asks for a url for a youtube playlist, parses out the videos from said playlist and adds them to a text file that you name
     url = input("Enter the youtube playlist url: ")
     fileName = input("Enter a file Name: ")
-    webpage = urlopen(url).read()
 
-    videos = str(webpage).split('data-video-id="')
-    file = open(CONST.SONGLISTDIR + "\%s.txt" % fileName, "w")
-    for url in videos[1:len(videos)-1]:   
-        rule = url.split('"')
-        title = url.split('data-title="')[1].split('"')[0].replace('[Copyright Free]','').replace('&amp;', '&').replace("\xe2\x80\x93", "-").replace("\xc3\xbc","u").replace("&#39;","'").replace("\xe2\x80\x93", "'")
-        finalURL = "https://www.youtube.com/watch?v=" + rule[0]
-        printed = "%s@%s\n" %(title.strip(),finalURL)
-        file.write(printed)
+    file = open(CONST.SONGLISTDIR + "/%s.txt" % fileName, "w")
+    for vid in getPlaylist(url):   
+        downloadMP3(vid, False, "/%s" % fileName)
+        file.write(video_title(vid))
     file.close()
                 
 def list_download(isHistory):           # Download from a list
@@ -247,16 +275,20 @@ def list_download(isHistory):           # Download from a list
     except NameError as e:
         print(e)
 
-def downloadMP3(sName, subFolder = ""):
-    query_string = urllib.parse.urlencode({"search_query" : sName + " Audio"})
-    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    url = 'http://www.youtube.com/watch?v=' + search_results[0]
+def downloadMP3(sName, prompt = True, subdir = ""):
+    if "www.youtube.com" in sName:
+        url = sName
+    else: 
+        queryString = urllib.parse.urlencode({"search_query" : sName + " Audio"})
+        url = getSingle("http://www.youtube.com/results?" + queryString)
+        
+    sName = video_title(url)
+
     options = {
         'format': 'bestaudio/best',
         'extractaudio' : True,  # only keep the audio
         'audioformat' : "mp3",  # convert to mp3 
-        'outtmpl': CONST.DIRECTORY + CONST.SUBDIR + '/%(title)s.mp3',    # name the file the Title of the video
+        'outtmpl': CONST.DIRECTORY + CONST.SUBDIR + '%s/%s.mp3' % (subdir, sName),    # name the file the Title of the video
         'noplaylist' : True,    # only download single song, not playlist
         }
     try:
@@ -265,7 +297,7 @@ def downloadMP3(sName, subFolder = ""):
             info_dict = ydl.extract_info(url, download=False)
             songName = ''.join([i if ord(i) < 128 else ' ' for i in removeAudio(info_dict.get("title"))])
             if (not os.path.exists(CONST.DIRECTORY + CONST.SUBDIR + "\\" + songName + ".mp3")):
-                if (input("Download %s? (Y/N)" % songName)).upper() == 'Y':
+                if not prompt or (input("Download %s? (Y/N)" % songName)).upper() == 'Y':
                     ydl.download([url])  
                     songLog = open(os.getcwd() + "\\" + CONST.LOG, 'a')
                     print(songName)
@@ -283,8 +315,6 @@ def single_name_download(song = "", toMain = True):     # download directly with
     if song == "":
         print(bcolors.WHITE + 'Enter the song name or full youtube link: ')  # get the song name from user
         song = input('>>> ')
-    if "www.youtube.com" in song: 
-        song = video_title(song)  
     if "feat." in song:
         song = song.split('(')[0]
     downloadMP3(song) 
@@ -295,7 +325,7 @@ def exit(code):                             # program exit
 def getHistory():
     global previouslyDownloaded 
     previouslyDownloaded = []
-    songLog = open(os.getcwd() + "\\" + CONST.LOG, 'r')
+    songLog = open(os.getcwd() + "/" + CONST.LOG, 'r')
     for songLink in songLog:
         if songLink.strip() != "": 
             sl = songLink.split('@')
@@ -383,10 +413,10 @@ def mainMenu():
                     if os.path.isdir(os.path.join(CONST.DIRECTORY, sng)):
                         suballs = os.listdir(os.path.join(CONST.DIRECTORY, sng))
                         for subsng in suballs:
-                            totalSize = totalSize + getSize(os.path.join(CONST.DIRECTORY, sng) + "\%s" % subsng)/1048576
+                            totalSize = totalSize + getSize(os.path.join(CONST.DIRECTORY, sng) + "/%s" % subsng)/1048576
                             totalCount = totalCount + 1
                     else:
-                        totalSize = totalSize + getSize(CONST.DIRECTORY + "\%s" % sng)/1048576
+                        totalSize = totalSize + getSize(CONST.DIRECTORY + "/%s" % sng)/1048576
                         totalCount = totalCount + 1
                 print("There are %s songs totalling to %s Mb of .mp3 files." % (totalCount, "{0:.2f}".format(totalSize)))
                 input()
